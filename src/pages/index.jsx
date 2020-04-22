f29657586976487f34326c9ec4eeab35efa624cb
Alexander <alexanderettema@gmail.com>
2020-04-22 20:50:54 +0200

    index deleted , copied about

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,32 @@
 import React from "react"
-import Title from "./title"
-import Wrapper from "./wrapper"
+import styled from 'styled-components'
 
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  color: FloralWhite;
+`;
 
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: ${props => props.inputColor || "Coral"};
+  margin: 4em;
+`;
 
-export default () => (
-  <div>
-      <Wrapper inputColor = "HotPink">
-     <Title inputColor = "FloralWhite" >Stay inspired, stay healthy</Title>
-     </Wrapper>
+function AboutPage(props) {
+  return (
+      <Wrapper inputColor = "Coral">
+    <Title>{props.titleProp}</Title>
+    </Wrapper>
+  )
+}
 
-     <Wrapper inputColor = "Grey">
-     <Title inputColor = "HotPink" >Stay inspired, stay healthy</Title>
-     </Wrapper>
+function AboutMe (){
+  return (
+    <AboutPage titleProp = "This is about me"></AboutPage>
+  )
+}
 
-  </div>
-)
+export default AboutMe
