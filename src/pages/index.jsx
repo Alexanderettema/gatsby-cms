0d589f3f00a47b4bcf860976a40bda5ea58bf8fe
Alexander <alexanderettema@gmail.com>
2020-04-23 20:15:19 +0200

    index with more components, removed inmport styled

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,38 @@
 import React from "react"
-import styled from 'styled-components'
+import Title from "./title"
+import Wrapper from "./wrapper"
 
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-  font-size: 2em;
-  text-align: center;
-  color: FloralWhite;
-`;
+function SectionOne(props) {
+    return (
+        <Wrapper inputColor = "HotPink">
+        <Title inputColor = "FloralWhite" >Stay really inspired, stay healthy</Title>
+        </Wrapper>
 
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: ${props => props.inputColor || "Coral"};
-  margin: 4em;
-`;
+    )
+  }
 
-function AboutPage(props) {
-  return (
-      <Wrapper inputColor = "Coral">
-    <Title>{props.titleProp}</Title>
-    </Wrapper>
-  )
-}
+  function SectionTwo(props) {
+    return (
+        <Wrapper inputColor = "Grey">
+        <Title inputColor = "HotPink" >Stay inspired, stay healthy</Title>
+        </Wrapper>
 
-function AboutMe (){
-  return (
-    <AboutPage titleProp = "This is about me"></AboutPage>
-  )
-}
+    )
+  }
 
-export default AboutMe
+export default () => (
+  <div>
+      <Wrapper inputColor = "HotPink">
+     <Title inputColor = "FloralWhite" >Stay inspired, stay healthy</Title>
+     </Wrapper>
+
+     <Wrapper inputColor = "Grey">
+     <Title inputColor = "HotPink" >Stay inspired, stay healthy</Title>
+     </Wrapper>
+
+     <SectionOne></SectionOne>
+    <SectionTwo></SectionTwo>
+    <SectionOne></SectionOne>
+    <SectionTwo></SectionTwo>
+  </div>
+)
